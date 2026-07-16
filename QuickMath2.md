3474fea239e42a53543912e83a6e51a747668206
ezrahunter-ai <ezra.hunter@hgs.hiddengeniusproject.org>
2026-07-16 12:02:58 -0400

    Add files via upload

---
+++ QuickMath2.md
@@ -0,0 +1,4 @@
+A function is a reusable block of code that performs a specific task.
+The keyword used to define a function is: def
+A return statement sends a value back to where the function was called.
+Parameters are the variables listed in the function definition. Arguments are the actual values you give the function when you call it.
---
+++ QuickMath2.py
@@ -0,0 +1,22 @@
+def add_sum(num1, num2):
+	return num1 + num2
+
+
+print(add_sum(10, 5))
+
+def AddSum(num1, num2):
+    return num1 + num2
+
+def Subtract(num1, num2):
+    return num1 - num2
+
+def Divide(num1, num2):
+    return num1 / num2
+
+def Multiply(num1, num2):
+    return num1 * num2
+
+print("Addition:", AddSum(10, 5))
+print("Subtraction:", Subtract(10, 5))
+print("Division:", Divide(10, 5))
+print("Multiplication:", Multiply(10, 5))
